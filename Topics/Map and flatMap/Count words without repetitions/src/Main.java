5d88060b48bc721a60f7e260b1631e512a46e9ee
Roman92P <roman.paszkow@gmail.com>
2022-10-24 15:26:38 +0200

    stream tasks

--- Topics/Map and flatMap/Count words without repetitions/src/Main.java
+++ Topics/Map and flatMap/Count words without repetitions/src/Main.java
@@ -24,6 +24,7 @@ public class Main {
 
     // Don't change the code below
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
 
