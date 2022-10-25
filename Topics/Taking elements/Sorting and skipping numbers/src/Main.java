1df96f444c26a7c1fb1ca993251b8b067a8c4695
Roman92P <roman.paszkow@gmail.com>
2022-10-25 15:38:28 +0200

    stream tasks

---
+++ Topics/Taking elements/Sorting and skipping numbers/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class ProcessNumbers {
+
+    public static List<Integer> processNumbers(Collection<Integer> numbers) {
+        return numbers.stream()
+                .sorted()
+                .dropWhile(integer -> integer < 10)
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Collection<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(HashSet::new));
+
+        String result = processNumbers(numbers).stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(" "));
+
+        System.out.println(result);
+    }
+}
