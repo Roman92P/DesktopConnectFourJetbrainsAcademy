e6a473ac60e4d8017a8555a3f26aba59dc190570
Roman92P <roman.paszkow@gmail.com>
2022-10-25 11:14:41 +0200

    stream tasks

---
+++ Topics/Infinite streams/Powers of two/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class StreamUtils {
+
+    public static Stream<Integer> generateStreamWithPowersOfTwo(int n) {
+        return Stream;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
