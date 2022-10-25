e4eb6e567292f4e3ee8d1d111fe9fd5c41240ee4
Roman92P <roman.paszkow@gmail.com>
2022-10-25 12:31:41 +0200

    stream tasks

--- Topics/Infinite streams/Powers of two/src/Main.java
+++ Topics/Infinite streams/Powers of two/src/Main.java
@@ -5,10 +5,15 @@ import java.util.stream.Stream;
 class StreamUtils {
 
     public static Stream<Integer> generateStreamWithPowersOfTwo(int n) {
-        return Stream;
+        int r = (int) Math.pow(2, n);
+        return Stream.iterate(1, x -> x < r, x -> x * 2);
     }
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int inputPow = scanner.nextInt();
+        Stream<Integer> result = generateStreamWithPowersOfTwo(inputPow);
+        String str = result.map(String::valueOf).collect(Collectors.joining(" "));
+        System.out.println(str);
     }
 }
