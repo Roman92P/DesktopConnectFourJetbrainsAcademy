4ded523ffacd9b8ed448a7a8007e6761c1522450
Roman92P <roman.paszkow@gmail.com>
2022-11-03 21:46:14 +0100

    jetbrains tasks

---
+++ Topics/Grouping collectors/Palindrome or not/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+class CollectorPalindrome {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] words = scanner.nextLine().split(" ");
+
+        Map<Boolean, List<String>> palindromeOrNo = Arrays.stream(words)
+                .collect(
+                        Collectors.partitioningBy(word -> {
+                            StringBuilder stringBuilder = new StringBuilder();
+                            stringBuilder.append(word);
+                            stringBuilder.reverse();
+                            String s = stringBuilder.toString();
+                            return s.equals(word);
+                        })
+                );
+
+        palindromeOrNo = new LinkedHashMap<>(palindromeOrNo);
+
+        System.out.println(palindromeOrNo);
+    }
+}
